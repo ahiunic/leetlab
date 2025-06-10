c4862de0bf7df4a87eddd25c766b657c2a1ea246
ahiunic <78784869+ahiunic@users.noreply.github.com>
2025-06-10 23:00:06 +0530

    Updated problem and homepage logic; added playlist & store components

--- frontend/src/components/CreateProblemForm.jsx
+++ frontend/src/components/CreateProblemForm.jsx
@@ -18,7 +18,6 @@ import {axiosInstance} from "../lib/axios"
 import toast from "react-hot-toast";
 import {useNavigate} from "react-router-dom";
 
-
 const problemSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
   description: z.string().min(10, "Description must be at least 10 characters"),
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,3 +4,11 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
   withCredentials: true,
 });
+
+
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: "/api/v1", 
+//   withCredentials: true,
+// });
